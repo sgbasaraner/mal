6b2ea9b7e3c7ee168d1068d63bb1a73bd2b8d423
Sarp Basaraner <sarpbasaraner@gmail.com>
2020-03-28 10:27:07 +0300

    step 0 naive impl

--- impls/rust/step0_repl.rs
+++ impls/rust/step0_repl.rs
@@ -1,3 +1,17 @@
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    unimplemented!();
+    loop {
+        print!("user> ");
+        io::stdout().flush();
+        let input = io::stdin().lock().lines().next().unwrap().unwrap();
+        println!("{}", rep(input));
+    }
+}
+
+fn read(val: String) -> String { val }
+fn eval(val: String) -> String { val }
+fn print(val: String) -> String { val }
+fn rep(val: String) -> String {
+    print(eval(read(val)))
 }
